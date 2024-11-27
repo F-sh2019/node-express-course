6c5e88dbd430d847afce8b53ed67e89b689dda6f
Filicity SH <f.elena.sh@gmail.com>
2024-11-26 21:08:58 -0800

    Cookie set

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,19 +4,57 @@ const { products, people } = require('./data');
 
 const express= require('express') ;
 const logger=require('./logger') ;
+const cookieParser= require('cookie-parser');
 const app=express() ;
 
 app.use(logger) ;
 
 app.use(express.json()) 
 app.use(express.urlencoded({extended: false}))
-
+app.use(cookieParser());
 
 app.use(express.static('./methods-public'));
 
 const peopleRouter = require('./routes/people');
 
 app.use('/api/v1/people', peopleRouter);
+
+
+
+
+const auth= (req , res , next)=>{
+    if(req.cookies.name ){
+        req.user=req.cookies.name 
+        next()
+    }
+    else{
+        return res.status(401).json({ success: false, message: 'Unauthorized' });
+    }
+}
+
+app.post("/logon" , (req,res)=>{
+  const { name } = req.body;
+  if (name) {
+    res.cookie("name", name); 
+    res.status(201).json({ message: `Hello, ${name}!` });
+  } else {
+    res.status(400).json({ message: "Name is required" });
+  }
+})
+
+app.delete("/logoff", (req,res)=>{
+    res.clearCookie("name")
+    res.status(200).json({message:'The User Logged OFF'})
+})
+
+app.get("/test", auth, (req, res) => {
+    res.status(200).json({ message: `Welcome, ${req.user}!` });
+  });
+
+
+
+//  . Then test this with Postman. You should be able to logon, logoff, and test, and the test should do something different depending on whether or not you are logged in.
+
 
 
 
