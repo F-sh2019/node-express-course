48c244a1b4cbd90c59f2305a3e4c6ec339263613
Filicity SH <f.elena.sh@gmail.com>
2024-12-10 22:25:49 -0800

    Week5

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,35 @@
+const connecDB = require('./db/connection')
+require('dotenv').config()
+const express=require('express')
+
+const app=express() 
+const  tasks  = require('./routes/tasks')
+
+//middleware
+
+app.use(express.json())
+
+//rout
+
+app.get('/hello' ,
+    (req, res)=>{
+res.send('Task manager app')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port=3000 
+const start = async()=>{
+    try{
+        console.log("connectdb")
+        await connecDB(process.env.MONGO_URI)
+        app.listen(port, () => console.log(`server is listening on port ${port} ...`));
+    }
+    catch(error){
+        console.log(error)
+    }
+
+}
+start()
+
 console.log('Task Manager App')
