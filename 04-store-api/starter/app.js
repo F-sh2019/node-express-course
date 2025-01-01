d92034143d6693cf8d8aada11cdf2b61ae2aa072
Filicity SH <f.elena.sh@gmail.com>
2024-12-31 17:57:41 -0800

    week 7 :using query parameter

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,45 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+require('express-async-errors')
+
+const express= require('express') ;
+const app= express() ;
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware=require('./middleware/error-handler')
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+//middleware 
+app.use(express.json())
+
+//routers
+
+app.get('/' , (req, res)=>{
+    res.send('<h1>Store API</h1><a href="/api/v1/products"> products rout</a>')
+})
+
+
+
+app.use('/api/v1/products' , productsRouter)
+
+//product route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+
+const port=process.env.Port ||3000 
+
+const start= async ()=>{
+    try {
+        //connectDb
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port , console.log(`Server is Listening port ${port}...`)) ;
+    } catch (error) {
+        console.log(error)
+ 
+    }
+}
+
+start()
